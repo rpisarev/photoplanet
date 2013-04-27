01c0062629bc81064236b6c0068d44e601c0ec95
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-04-27 21:00:42 +0300

    Create templates tag %server_time% for display current time on server like tag %now% (see http://djbook.ru/ch10s04.html)

---
+++ photoplanet/servtime/templatetags/servertime.py
@@ -0,0 +1,20 @@
+from django import template
+import datetime
+
+
+register = template.Library()
+
+class ServerTime(template.Node):
+
+    def __init__(self):
+        self.format_time = '%I:%M %a, %Y-%m-%d'
+
+    def render(self, context):
+        now = datetime.datetime.now()
+        return now.strftime(self.format_time)
+
+
+@register.tag(name="server_time")
+def server_datetime(parser, token):
+#    tag_name, format_string = token.split_contents()
+    return ServerTime()
