5b471bb7facff52e54bd166b343c3d1f1df76211
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-05-11 21:58:05 +0300

    Fix setting.py for static in heroku

--- photoplanet/photoplanet/settings.py
+++ photoplanet/photoplanet/settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = normpath(join(SITE_ROOT, 'statix'))
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
