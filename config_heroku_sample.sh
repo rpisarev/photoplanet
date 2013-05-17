f14781650b4ef3f8db8747c88d9d37787564b241
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-05-17 15:34:28 +0300

    Config for Heroku

--- config_heroku_sample.sh
+++ config_heroku_sample.sh
@@ -3,3 +3,4 @@
 heroku config:set DJANGO_SETTINGS_MODULE=photoplanet.settings.heroku
 heroku config:set INSTAGRAM_CLIENT_ID=YOUR_INSTAGRAM_CLIENT_ID
 heroku config:set INSTAGRAM_CLIENT_SECRET=YOUR_INSTAGRAM_CLIENT_SECRET
+heroku config:set WEBSITE URL=http://rpisarev-photoplanet.herokuapp.com/
