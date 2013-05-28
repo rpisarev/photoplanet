cf1acb0d74051296bab15241a63fca290b14cf9e
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-05-28 17:41:43 +0300

    I HAD AJAX!!!!!

--- photoplanet/static/js/vote.js
+++ photoplanet/static/js/vote.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
     $('.js-vote').click(function(){
         var photo_id = $(this).attr('data-id');
         var vote = $(this).attr('data-vote');
+	alert(window.PHOTO.vote_url);
+	alert(photo_id);
+	alert(vote);
         $.ajax({
             url: window.PHOTO.vote_url,
             data: {'photo': photo_id, 'vote': vote},
             type: 'POST',
             success: function(data){
                 console.log(data);
+		alert(data);
                 $('.js-vote-' + photo_id).text(data.votes);
             }
 
