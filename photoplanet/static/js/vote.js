745a8ab7ff12c33363cbdde91b55439e219e2552
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-05-30 08:43:25 +0300

    AJAX is work! HAHAHA!!!

--- photoplanet/static/js/vote.js
+++ photoplanet/static/js/vote.js
@@ -7,10 +7,6 @@ $(document).ready(function(){
             url: window.PHOTO.vote_url,
             data: {'photo': photo_id, 'vote': vote},
             type: 'POST',
-            beforeSend: function(xhr, settings) {
-               xhr.setRequestHeader("X-CSRFToken", csrf_token);
-            },
-
             success: function(data){
                 console.log(data);
 		alert(data);
