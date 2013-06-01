2bd1b63edb8127ac8ad293012263aa2b1c5d4480
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2013-06-01 10:30:56 +0300

    Add fields (create and like as unchangeble) to display in Admin

--- photoplanet/photoplanet/admin.py
+++ photoplanet/photoplanet/admin.py
@@ -3,6 +3,18 @@ from .models import Photo, Vote
 
 
 class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        '__unicode__',
+        'photo_id',
+        'username',
+        'user_avatar_url',
+        'photo_url',
+        'created_time',
+        'like_count',
+        'vote_count',
+        'is_spam'
+    )
+    readonly_fields = ['created_time', 'like_count',]
     search_fields = ['name']
 
 admin.site.register(Photo, PhotoAdmin)
